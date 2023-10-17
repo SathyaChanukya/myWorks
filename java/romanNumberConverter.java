ca4c07ef658204fa08ee72528270fe29867ef0ed
SathyaChanukya <83706303+SathyaChanukya@users.noreply.github.com>
2023-10-17 16:13:14 +0530

    romanNumberConverter.java

---
+++ java/romanNumberConverter.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class random {
+    public static void main(String[] args) {
+        // Create a HashMap to store Roman numeral values
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().toUpperCase();
+
+        int result = 0;
+        int previousValue = 0;
+
+        for (int i = romanNumeral.length() - 1; i >= 0; i--) {
+            char currentChar = romanNumeral.charAt(i);
+            int currentValue = romanMap.get(currentChar);
+            if (currentValue < previousValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+            previousValue = currentValue;
+        }
+      
+        System.out.println("Integer representation of Roman numeral " + romanNumeral + " is: " + result);
+    }
+}
