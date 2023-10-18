b65bcda48090e6439b58530a3af1fae56f78fe9a
SathyaChanukya <83706303+SathyaChanukya@users.noreply.github.com>
2023-10-18 14:58:12 +0530

    sortDESC.js

---
+++ javascript/sortDESC.js
@@ -0,0 +1,16 @@
+let Sort=(arr)=>{
+    let noBreaks=true
+    for(let i=arr.length;i>0;i--){
+        for(let j=0;j<i-1;j++){
+            if(arr[j]<arr[j+1]){
+                let temp=arr[j+1]
+                arr[j+1]=arr[j]
+                arr[j]=temp
+                noBreaks=false
+            }
+        }
+        if(noBreaks)break
+    }
+    return arr
+}
+Sort([1,2,3,4,5])
